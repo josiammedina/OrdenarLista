a91c7b1e5accd12abc280eea915b1f0a8afda316
Jose <73659054+josiammedina@users.noreply.github.com>
2020-11-03 15:28:05 -0400

    Update Seminario_1_Listas.cpp

--- Seminario_1_Listas.cpp
+++ Seminario_1_Listas.cpp
@@ -1,7 +1,10 @@
-//1er Seminario Estructura de Datos
-// Ordenar listas, Equipo II
-// OJO ESTOY TRABAJANDOLO POR PARTE y lo estare publicando segun voy probandolo, Muchas gracias CompaÃ±eros
-#include <iostream>
+//Estructura de Datos y Algoritmos ISW-305
+//Facilitador Maikel Aparicio
+//1er Seminario, Ordenar listas, Equipo II
+//Sustentado por: Abraham Pineda, Teddy Paulino, Jose Ruiz y Jose Anibal Medina
+
+#include <iostream>				//Declaracion de las librerias que estaremos utilizando
+#include <stdlib.h>		
 
 using namespace std;
 
@@ -9,4 +12,16 @@ struct nodo {
 	int dato;				//En dicha variable guardaremos los contenidos de los nodos
 	struct nodo *siguiente;			//Este es el puntero Siguiente
 				
-}
+};
+
+main(){
+	struct nodo *primero;			//Asignado al primer nodo
+	struct nodo *actual	;		//Asignado a los nuevos nodos que se van creando
+	primero = NULL;				//Declaramos primero como NULL
+	int dato;				//Variable donde almanecaremos los datos 
+	int cant;
+	int i=1;
+	cout <<"Digitar cantidad de nodos: ";	//Imprimir por pantalla pedido de digitar cantidad de nodos
+	cin>>cant;
+	
+};
