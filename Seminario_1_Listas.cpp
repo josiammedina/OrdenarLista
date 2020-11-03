855e50d9ddf57cba3d7aa89b3e19762080d02202
Jose <73659054+josiammedina@users.noreply.github.com>
2020-11-03 16:49:45 -0400

    Update Seminario_1_Listas.cpp

--- Seminario_1_Listas.cpp
+++ Seminario_1_Listas.cpp
@@ -16,24 +16,26 @@ struct nodo {
 main(){
 	struct nodo *primero;			//Asignado al primer nodo
 	struct nodo *actual	;		//Asignado a los nuevos nodos que se van creando
-	primero = NULL;				//Declaramos primero como NULL
+	primero = NULL;				//Declaramos PRIMERO como NULL
 	int dato;				//Variable donde almanecaremos los datos 
-	int cant;
-	int i=1;
+	int cant;				//Variable donde solicita la cantidad de datos a digitar
+	int i=1;				//Variable del contador que crea los nodos
 	cout <<"Digitar cantidad de nodos: ";	//Imprimir por pantalla pedido de digitar cantidad de nodos
-	cin>>cant;
+	cin>>cant;				//Solicitamos la cantidad de nodos que se desea
 	
 	while(i<=cant){
-		actual=(struct nodo*)malloc(sizeof(struct nodo));
-		actual->siguiente=primero;			//
-		cout<<"Digitar datos: ";			//Imprimir por pantalla pedido de digitar datos
-		cin>>dato;					//
-		actual->num=dato;				//
-		primero=actual;					//
-		i++;
-	}		
-	cout<<"Listado de Datos: ";				//Imprimimos por pantalla titulo Listado de Datos
-	while(actual!=NULL){					//El apuntador nodo ira al siguiente hasta encontrar NULL y detenerse
+	actual=(struct nodo*)malloc(sizeof(struct nodo));
+	actual->siguiente=primero;		//Su finalidad es que actual tome el valor de primero para ir creando los demas nodos de forma ordenada 
+	cout<<"Digitar datos: ";		//Imprimir por pantalla pedido de digitar datos
+	cin>>dato;				//Solicitamos de entrada de datos para los nodos
+	actual->num=dato;			//Se pasa el dato digitado a la variable NUM
+	primero=actual;				//El apuntador PRIMERO pasa a la posicion de ACTUAL
+	i++;
+	}
+	
+	//Linea de Codigo que nos permitira imprimir los datos digitados
+	cout<<"Listado de Datos: ";		//Imprimimos por pantalla titulo Listado de Datos asi como cada uno de los datos digitados.
+	while(actual!=NULL){			//El apuntador nodo ira al siguiente hasta encontrar NULL y detenerse
 	cout << actual->num << ", ";	
 	actual=actual->siguiente;
 	}
