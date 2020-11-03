798ea0eef855ba18376dae0b1c0c8bae7dff1eb9
Jose <73659054+josiammedina@users.noreply.github.com>
2020-11-03 11:36:57 -0400

    Add files via upload

---
+++ Seminario_1_Listas.cpp
@@ -0,0 +1,29 @@
+//1er Seminario Estructura de Datos
+// Ordenar listas, Equipo II
+
+#include <iostream>
+
+using namespace std;
+
+struct nodo {
+	int dato;
+	nodo *siguiente;
+	*primero, *ultimo;
+}
+
+void insertarNodo ();
+
+int main (){
+	return 0;	
+}
+
+void insertarNodo ();{
+	nodo* nuevo = new nodo();
+	cout << "Ingresar Datos: ";
+	cin >> nuevo->dato ;
+	if(primero == NULL){
+		
+	}else{
+		
+	}
+}
